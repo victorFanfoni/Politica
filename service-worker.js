211bea0957f5ec6c3bfca59b5a6e3b1f9b741989
victorFanfoni <126927982+victorFanfoni@users.noreply.github.com>
2026-01-08 21:27:49 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,35 +1,39 @@
 const CACHE_NAME = "explore-tupa-v1";
 
+// Use caminhos relativos (sem a barra inicial) para maior compatibilidade
 const FILES_TO_CACHE = [
-  "/",
-  "/index.html",
-  "/contato.html",
-  "/raizes.html",
-  "/trilhas.html",
-  "/legado.html",
-  "/manifest.json",
-
-  // imagens principais
-  "/imgs/igreja_matriz_de_sao_pedro_em_tupa.jpg",
-  "/imgs/bg_btn_historia_tupa.png",
-  "/imgs/legado.png",
-  "/imgs/bg_btn_trilha.png",
-  "/imgs/bg_btn_fotos_antigas.png",
-  "/imgs/bg_btn_curiosidades.png",
-  "/imgs/bg_btn_rota_cultural.png",
-  "/imgs/bg_btn_hospedagem.png",
-  "/imgs/bg_btn_alimentacao.png",
-  "/imgs/bg_btn_lenda_quiz.png"
+  "./",
+  "./index.html",
+  "./contato.html",
+  "./raizes.html",
+  "./trilhas.html",
+  "./legado.html",
+  "./manifest.json",
+  "./imgs/igreja_matriz_de_sao_pedro_em_tupa.jpg",
+  "./imgs/bg_btn_historia_tupa.png",
+  "./imgs/legado.png",
+  "./imgs/bg_btn_trilha.png",
+  "./imgs/bg_btn_fotos_antigas.png",
+  "./imgs/bg_btn_curiosidades.png",
+  "./imgs/bg_btn_rota_cultural.png",
+  "./imgs/bg_btn_hospegem.png", // Ajustado para bater com o nome no seu HTML
+  "./imgs/bg_btn_alimentacao.png",
+  "./imgs/bg_btn_lenda_quiz.png"
 ];
 
-// INSTALA
+// INSTALAÇÃO
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log("Caching assets...");
+        return cache.addAll(FILES_TO_CACHE);
+      })
+      .catch(err => console.error("Erro no Cache AddAll: ", err))
   );
 });
 
-// ATIVA
+// ATIVAÇÃO
 self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys().then(keys =>
@@ -38,13 +42,14 @@ self.addEventListener("activate", event => {
       )
     )
   );
+  return self.clients.claim();
 });
 
-// FETCH (offline)
+// BUSCA (FETCH)
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response =>
-      response || fetch(event.request)
-    )
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
